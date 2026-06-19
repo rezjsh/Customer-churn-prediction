2fca8fe75831548e9cf155ab22af6bce44b9c92b
rezjsh <149146755+rezjsh@users.noreply.github.com>
2026-06-19 09:55:11 +0330

    Implement DataIngestionTrainingPipeline class for data ingestion process

---
+++ src/customer_churn/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,24 @@
+from customer_churn.config.configuration import ConfigurationManager
+from customer_churn.components.data_ingestion import DataIngestion
+from customer_churn.utils.logging_setup import logger
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def run(self):
+        config_manager = ConfigurationManager()
+        data_ingestion_config = config_manager.get_data_ingestion_config()
+        
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        raw_dataframe = data_ingestion.download_data()
+        logger.info("Data ingestion completed successfully.")
+        logger.info(f"Raw data shape: {raw_dataframe.shape}")
+        logger.info(f"Raw data columns: {raw_dataframe.columns.tolist()}")
+        logger.info(f"Raw data preview:\n{raw_dataframe.head()}")
+        logger.info(f"Raw data info:\n{raw_dataframe.info()}")
+        logger.info(f"Raw data description:\n{raw_dataframe.describe()}")
+        
+        
+        # Optionally pass this DataFrame forward or cache it to data/raw/
+        return raw_dataframe
