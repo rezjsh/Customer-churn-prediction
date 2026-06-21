70d0222be186c6426893687aad67275bd82b7370
rezjsh <149146755+rezjsh@users.noreply.github.com>
2026-06-21 10:13:13 +0330

    Add EDA and data validation configurations to ConfigurationManager

--- src/customer_churn/config/configuration.py
+++ src/customer_churn/config/configuration.py
@@ -1,12 +1,13 @@
-from customer_churn.constants.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
-from customer_churn.utils.common import read_yaml_file  
-from customer_churn.entity.config_entity import DataIngestionConfig
+from customer_churn.constants.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH, SCHEMA_FILE_PATH
+from customer_churn.utils.common import create_directory, read_yaml_file  
+from customer_churn.entity.config_entity import DataIngestionConfig, DataValidationConfig, EDAConfig
 from pathlib import Path
 
 class ConfigurationManager:
-    def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
+    def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH, schema_filepath = SCHEMA_FILE_PATH):
         self.config = read_yaml_file(Path(config_filepath))
         self.params = read_yaml_file(Path(params_filepath))
+        self.schema = read_yaml_file(Path(schema_filepath))
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
@@ -16,4 +17,35 @@ class ConfigurationManager:
             source_type=config.source_type,
             file_path=Path(config.file_path),
             file_type=config.file_type
+        )
+    
+    def get_eda_config(self) -> EDAConfig:
+        config = self.config.data_eda
+        params = self.params.eda_params
+
+        create_directory([config.root_dir])
+
+        return EDAConfig(
+            root_dir=Path(config.root_dir),
+            data_path=Path(config.data_path),
+            json_report_path=Path(config.json_report_path),
+            text_report_path=Path(config.text_report_path),
+            target_column=params.target_column,
+            correlation_threshold=params.correlation_threshold,
+            outlier_method=params.outlier_method
+        )
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.columns
+
+        dirs_to_create = [config.root_dir]
+        create_directory(dirs_to_create)
+
+        return DataValidationConfig(
+            root_dir=Path(config.root_dir),
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=Path(config.unzip_data_dir),
+            all_schema=schema,
         )
--- src/customer_churn/entity/config_entity.py
+++ src/customer_churn/entity/config_entity.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import List
+
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -7,3 +9,22 @@ class DataIngestionConfig:
     source_type: str
     file_path: Path
     file_type: str
+
+
+@dataclass(frozen=True)
+class EDAConfig:
+    root_dir: Path
+    data_path: Path
+    json_report_path: Path
+    text_report_path: Path
+    target_column: str
+    correlation_threshold: float
+    outlier_method: str
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
